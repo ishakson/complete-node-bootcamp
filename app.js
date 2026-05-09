6bc975fd6c9610a3e677cabfa6665ebc34b98a03
ultrabot <72345759+ishakson@users.noreply.github.com>
2026-05-09 16:59:40 +0000

    main

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs")
 
 const app = express();
+app.use(express.json())
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`))
 app.get("/api/v1/tours", (req, res)=>{
     res.status(200).json({
@@ -12,8 +14,21 @@ app.get("/api/v1/tours", (req, res)=>{
     })
 })
 
-const port = 3000;
+app.post("/api/v1/tours", (req,res)=>{
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId},req.body)
+    tours.push(newTour)
 
+    fs.writeFile(`${__dirname}/dev-data/data/tours.json`,JSON.stringify(tours),err =>{
+        res.status(201).json({
+            status:"success",
+            data:{
+                tour: newTour
+            }
+        })
+     })
+})
+const port = 3000;
 app.listen(port,(req, res)=>{
     console.log("server is listening on port 3000!")
 })

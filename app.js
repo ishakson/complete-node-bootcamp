ff9d216ecf1df13cd0e0ba99b4c7590ffd92e460
ishak <ishaksylmz7@gmail.com>
2026-05-22 20:26:32 +0300

    main

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
+
 app.use(express.json());
-app.use((req,res,next)=>{
-  console.log("it done!")
-  next()
-})
-app.use((req,res,next) => {
+app.use((req, res, next) => {
+  console.log('it done!');
+  next();
+});
+app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  next()
-})
+  next();
+});
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours.json`),
 );
 const getAllTours = (req, res) => {
-  console.log(req.requestTime)
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     createdAt: req.requestTime,
@@ -83,6 +87,38 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req,res) =>{
+  return res.status(500).json({
+    status:"fail",
+    message:"not defined"
+  })
+}
+
+const createUser = (req,res) =>{
+  return res.status(500).json({
+    status:"fail",
+    message:"not defined"
+  })
+}
+const getUser = (req,res) =>{
+  return res.status(500).json({
+    status:"fail",
+    message:"not defined"
+  })
+}
+const updateUser = (req,res) =>{
+  return res.status(500).json({
+    status:"fail",
+    message:"not defined"
+  })
+}
+const deleteUser = (req,res) =>{
+  return res.status(500).json({
+    status:"fail",
+    message:"not defined"
+  })
+}
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -90,6 +126,14 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 const port = 3000;
 app.listen(port, (req, res) => {
   console.log('server is listening on port 3000!');

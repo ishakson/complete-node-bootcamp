4b4c31198c7a0b409dcf8fd8268d3b0626b9bafa
ishak <ishaksylmz7@gmail.com>
2026-05-24 23:24:14 +0300

    main

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const userRouter = require("./routes/userRoutes");
-const tourRouter = require("./routes/tourRoutes");
+
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
- 
-app.use((req, res, next) =>  {
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
   console.log('it done!');
   next();
 });
@@ -19,10 +21,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 module.exports = app;
-
-
-
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config({path: "./config.env"})
+
 const app = require("./app");
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+
+console.log(process.env)
 app.listen(port, (req, res) => {
   console.log('server is listening on port 3000!');
 });
